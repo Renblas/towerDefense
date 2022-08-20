026f05cfd58cbe8d849734e8a18a593c829a8b3f
Renblas <renblas.dev@gmail.com>
2022-08-19 20:04:34 -0400

    new

--- scripts/index.js
+++ scripts/index.js
@@ -4,21 +4,28 @@
 const FPS_TARGET = 30;
 const UPS_TARGET = 120;
 const UPS_perFrameNum = UPS_TARGET / FPS_TARGET;
+const RUNNING_IN_APP = !(typeof window.AppInventor === "undefined");
 
 let gameState = "mainMenu";
 let gamePaused = false;
 
 // Setup
 function setup() {
-    createCanvas(windowWidth, windowHeight);
+    if (RUNNING_IN_APP) {
+        createCanvas(windowWidth, windowHeight);
+        print("hi")
+    } else {
+        createCanvas(windowWidth, (windowWidth / 19) * 9);
+    }
+
     frameRate(FPS_TARGET);
 }
 
 // Draw
 function draw() {
     // Check if in Current Game 
-    if (gameState == "game") {
-        if (gamePaused == false) {
+    if (gameState === "game") {
+        if (gamePaused === false) {
             for (let i = 0; i < UPS_perFrameNum; i++) {
                 GameUpdate()
             }
@@ -30,7 +37,9 @@ function draw() {
     }
 
     // Main Menu
-
+    if (gameState === "mainMenu") {
+        background(155)
+    }
 
 
 }
